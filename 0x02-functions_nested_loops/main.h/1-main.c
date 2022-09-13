b794fb9f214154c3bcb4fe7ebbbcc87a05ee3268
adeniyipeter <88614377+adeniyipeter@users.noreply.github.com>
2022-09-13 18:31:19 +0100

    Update 1-main.c

--- 0x02-functions_nested_loops/main.h/1-main.c
+++ 0x02-functions_nested_loops/main.h/1-main.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * main - check the code for Holberton School students.
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    print_alphabet();
-    return (0);
+	print_alphabet();
+	return (0);
 }
