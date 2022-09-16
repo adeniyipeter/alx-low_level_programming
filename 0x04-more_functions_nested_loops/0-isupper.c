cf647a27003164f34c2b73011e9b2d1f8e487ad2
adeniyipeter <adeniyipeter999@gmail.com>
2022-09-15 19:45:03 -0700

    my first commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,8 +2,8 @@
 
 /**
  * _isupper - a function that checks for uppercase character
- * @c: input
- * Return: 1 if c is uppercase, 0 otherwise
+ *  @c: input
+ *  Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
