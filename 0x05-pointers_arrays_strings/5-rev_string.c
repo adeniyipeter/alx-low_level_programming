dec9546725060243452f867ee0aaa3a0d7585f0a
adeniyipeter <88614377+adeniyipeter@users.noreply.github.com>
2022-09-20 03:39:52 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * rev_string - Reverse a string
- * @s: String to reverse
- * Return: Nothing
- */
+*rev_string - Reverse a string
+*@s: String to reverse
+*Return: Nothing
+*/
 void rev_string(char *s)
 {
 int i = 0, length;
@@ -17,9 +17,9 @@ length--;
 }
 
 /**
- * _strlen - returns the length of a string
+ *_strlen - returns the length of a string
  * @s: string
- * Return: returns lenght;
+ *Return: returns lenght;
  */
 int _strlen(char *s)
 {
@@ -28,15 +28,15 @@ inc = 0;
 for (count = 0; s[count] != '\0'; count++)
 inc++;
 
-return (inc)
+return (inc);
 }
 
 /**
- * swap_char - swap two characters
- * @a: first character
- * @b: second character
- * Return: nothing
- */
+*swap_char - swap two characters
+*@a: first character
+*@b: second character
+*Return: nothing
+*/
 
 void swap_char(char *a, char *b)
 {
