53698e261bc1d19aa44ce819bf9626a3128c4a94
adeniyipeter <88614377+adeniyipeter@users.noreply.github.com>
2022-09-27 14:36:58 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,43 +1,18 @@
+#include <string.h>
 #include "main.h"
+
 /**
- * _strstr - prints the consecutive caracters of s1 that are in s2.
- * @haystack: source string
- * @needle: searching string
- *
- * Return: new string.
- */
+  * _strstr - Locates the first occurance of needle in haystack
+  * @haystack: String to search through
+  * @needle: String to locate
+  * Return: A pointer to the beginning of the located
+  *	substring, or NULL if the substring is not
+  *	found
+  */
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack)
-	{
-		if ((*haystack == *needle && coincidence(haystack, needle) == 1) || !*needle)
-		{
-			return (haystack);
-		}
-		else
-		{
-			haystack++;
-		}
-	}
-	return (0);
-}
-/**
- * coincidence - define if the string b is inside a.
- * @a: source string
- * @b: string to be searched
- *
- * Return: 1 if there is coincidence, otherwise 0.
- */
-int coincidence(char *a, char *b)
-{
-	while (*b && *b == *a)
-	{
-		b++;
-		a++;
-	}
+	char *locSubstr;
 
-	if (*b == '\0')
-		return (1);
-	else
-		return (0);
+	locSubstr = strstr(haystack, needle);
+	return (locSubstr);
 }
